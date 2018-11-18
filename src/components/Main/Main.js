22b6475213cf49b6ac23e7b807973434a2d31e56
Hewon Jeong <hewon.jng@gmail.com>
2018-11-18 23:53:31 +0900

    cleanup Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react'
 
-class Main extends Component {
-    state = {
-    }
+const Main = () => (
+  <div className="main">
+    <p>
+      Hi! I’m{' '}
+      <span>
+        <a href="#">Dahye</a>
+      </span>
+      , a digital product designer who likes to weave vague ideas and create
+      working products.
+    </p>
+  </div>
+)
 
-    render() {
-        return (
-            <div className="main">
-                <p>
-                    Hi! I’m <span><a href="">Dahye</a></span>,
-a digital product designer who likes to weave vague ideas and create working products.
-          </p>
-            </div>)
-
-    }
-}
-
-export default Main;
+export default Main
